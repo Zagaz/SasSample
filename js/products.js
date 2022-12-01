d06dcdc5005449931916cc205a487da838a26634
“André <andre_ranulfo@hotmail.com>
2022-11-30 23:25:40 -0300

    Products card styles and flexbox

--- js/products.js
+++ js/products.js
@@ -11,62 +11,51 @@ function myProducts(){
         const divProducts = document.querySelector("#products");
 
         data.map((item) => {
-            //card
-            card = document.createElement("div");
-            card.setAttribute ('id' ,"card_" + item.id)
-            card.setAttribute ('class' , "card")
-
-            //card-image-Container
-            imageContainer = document.createElement("div");
-            imageContainer.setAttribute ('class' , "image-container")
-            
-            //Thumb
-            thumb = document.createElement ("img");
-            thumb.setAttribute ('id' , "img_" + item.id)
-            thumb.setAttribute ('src' , item.image)
-            thumb.setAttribute ('class' , "thumbnail img-fluid rounded thumbnail-image")
-
-            //product-detail-container
-
-            card = document.createElement("div");
-            card.setAttribute ('id' ,"card_" + item.id)
-            card.setAttribute ('class' , "card")
-
-
-
-
-
-
-            
-
-            // Title
-            title = document.createElement("h1")
-            title.setAttribute("id", "title_" + item.id)
-            title.setAttribute("class", "title")
-            title.innerHTML = item.title;
-            
-            // Description
-            description = document.createElement("p");
-            description.setAttribute("id", "description_" + item.id)
-            description.setAttribute("class", "description")
-            description.innerHTML = item.description;
-
-            //Price
-            price = document.createElement("h3");
-            price.setAttribute("id", "price_" + item.id)
-            price.setAttribute("class", "price")
-            price.innerHTML = item.price;
-
-
-            divProducts.appendChild(card);
-            card.appendChild(imageContainer)
-
           
-            card.appendChild(thumb)
-            card.appendChild(title)
-            card.appendChild(price)
-            //card.appendChild(description)
+    
+                
+                //card
+                card = document.createElement("div");
+                card.setAttribute ('class' , "itemContainer")
+                    
+                    // //thumbContainer
+                    // thumbContainer = document.createElement ("div");
+                    // thumbContainer.setAttribute ('id' , "tc_" + item.id)
+                    // thumbContainer.setAttribute ('class' , "thumbContainer")
+                    
+                        //Thumb
+                        thumb = document.createElement ("img");
+                        thumb.setAttribute ('id' , "img_" + item.id)
+                        thumb.setAttribute ('src' , item.image)
+                        thumb.setAttribute ('class' , "thumbnail img-fluid rounded thumbnail-image")
+                    
+                    //TitleContainer
+                    titleContainer = document.createElement ("div");
+                    titleContainer.setAttribute ('id' , "titlec_" + item.id)
+                    titleContainer.setAttribute ('class' , "titleContainer")
+                        // Title
+                        title = document.createElement("h1")
+                        title.setAttribute("id", "title_" + item.id)
+                        title.setAttribute("class", "title")
+                        title.innerHTML = item.title
+                    
+                    //PriceContainer
+                    priceContainer = document.createElement ("div");
+                    priceContainer.setAttribute ('id' , "price_" + item.id)
+                    priceContainer.setAttribute ('class' , "priceContainer")
+                        //Price
+                        price = document.createElement("h3");
+                        price.setAttribute("id", "price_" + item.id)
+                        price.setAttribute("class", "price")
+                        price.innerHTML = "$" + item.price
 
+                //Rendering
+                divProducts.appendChild(card);
+                 
+                    card.appendChild(thumb)  
+                    card.appendChild(title)
+                    card.appendChild(priceContainer)
+                        priceContainer.appendChild(price)
         })
 
         
